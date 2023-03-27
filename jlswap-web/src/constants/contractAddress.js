ff52dc00ff71d7ba5191fa78c14b9addbb60aeb2
sunjun <sunjun@nj.iscas.ac.cn>
2023-03-27 18:12:49 +0800

    fix: 修改地址

--- jlswap-web/src/constants/contractAddress.js
+++ jlswap-web/src/constants/contractAddress.js
@@ -1,4 +1,4 @@
 export default {
-    factory_address: '0x7bf94defc6b6171c170865f776b1481c24d69592',
-    router_address: '0x3ee33f132973778451f8eb8429ec289658416ab0'
+    factory_address: '0x5757371414417b8c6caad45baef941abc7d3ab32',
+    router_address: '0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff'
 }
